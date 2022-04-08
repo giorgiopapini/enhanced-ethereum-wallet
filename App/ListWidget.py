b2f9542e8bc75422fc58c71a06d160c1952861b0
Giorgio <giorgio.papini04@gmail.com>
2022-04-08 22:46:30 +0200

    Adding custom scrollable list for label display

---
+++ App/ListWidget.py
@@ -0,0 +1,42 @@
+from tkinter import *
+
+
+class ListWidget:
+
+    def __init__(self, parent=None, elements=None, **kwargs):
+        self.parent = parent
+        self.elements = elements
+        self.space_between = kwargs.get("space_between", 1)
+
+        self.canvas = Canvas(self.parent)
+        self.frame = Frame(self.canvas)
+
+        self.display_elements()
+
+        self.canvas.create_window(0, 0, anchor='nw', window=self.frame)
+        self.canvas.update_idletasks()
+
+        self.scrollbar = Scrollbar(self.parent, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(
+            scrollregion=self.canvas.bbox('all'),
+            yscrollcommand=self.scrollbar.set
+        )
+
+        self.canvas.place(
+            x=0, y=0
+        )
+        self.scrollbar.pack(fill='y', side='right')
+
+    def display_elements(self):
+        for element in self.elements:
+            label = Label(
+                self.frame,
+                image=element,
+            )
+            label.pack(
+                pady=self.space_between
+            )
+            label.bind("<Button>", self.clicked)
+
+    def clicked(self, event):
+        print(event.widget.cget("text"))
--- App/YourWallet/WalletPage.py
+++ App/YourWallet/WalletPage.py
@@ -1,26 +1,47 @@
 from tkinter import *
+
+from App.ListWidget import ListWidget
 from Page import Page
 
 
 class WalletPage(Page):
 
+    TOKEN_BG_IMG = "App/YourWallet/token_bg_img.png"
+
     def __init__(self, root, web3, **kwargs):
         super().__init__(root, web3, **kwargs)
 
-        self.wallet = Label(
+        self.wallet_eth_balance = Label(
             self.frame,
-            text="Sample Wallet"
+            text=f"Total ETH value: {round(self.eth_account.get_balance('ether'), 4)} ETH",
+            font=("Helvetica", 20)
         )
 
-        self.wallet.place(
-            x=150, y=150,
+        self.wallet_eth_balance.place(
+            x=40, y=40,
         )
 
-        self.bal = Label(
+        self.tokens = Label(
             self.frame,
-            text=f"balance: {round(self.eth_account.get_balance('ether'), 4)} eth"
+            text=f"Tokens:",
+            font=("Helvetica", 15)
         )
 
-        self.bal.place(
-            x=150, y=135,
+        self.tokens.place(
+            x=90, y=120
+        )
+
+        self.tokens_list_frame = Frame(self.frame)
+        self.tokens_list_frame.place(
+            x=20, y=165,
+            width=240,
+            height=270,
+        )
+
+        self.list = ListWidget(
+            parent=self.tokens_list_frame,
+            space_between=5,
+            elements=[
+                PhotoImage(file=self.TOKEN_BG_IMG)
+            ]
         )
