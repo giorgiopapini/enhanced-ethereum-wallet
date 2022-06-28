2ff07a219e484a7ac3c6708c19c2da5a9ed343be
Giorgio <giorgio.papini04@gmail.com>
2022-06-28 07:38:12 +0200

    Added UI interaction in 'MarketAnalysis' page

--- App/MarketAnalysis/AutomationPage.py
+++ App/MarketAnalysis/AutomationPage.py
@@ -17,10 +17,13 @@ class AutomationPage(Page):
 
     BACKGROUND = "App/MarketAnalysis/background.png"
     DONE_BTN_IMG = "App/MarketAnalysis/done_btn_img.png"
-    ID_FIELD_IMG = "App/MarketAnalysis/id_field_img.png"
+    FIELD_IMG = "App/MarketAnalysis/field_img.png"
 
     def __init__(self, root, web3, **kwargs):
         super().__init__(root, web3, **kwargs)
+
+        self.graph_frame = Frame(self.frame)
+        self.price_chart = None
 
         self.canvas = Canvas(
             self.frame,
@@ -38,9 +41,9 @@ class AutomationPage(Page):
             image=self.background_img
         )
 
-        self.id_field_img = PhotoImage(file=self.ID_FIELD_IMG)
-        self.id_field = self.canvas.create_image(
-            160.5, 139.5,
+        self.id_field_img = PhotoImage(file=self.FIELD_IMG)
+        self.id_field_bg = self.canvas.create_image(
+            133.0, 139.5,
             image=self.id_field_img
         )
 
@@ -55,7 +58,28 @@ class AutomationPage(Page):
 
         self.id_field.place(
             x=72.5, y=127,
-            width=176.0,
+            width=121.0,
+            height=27
+        )
+
+        self.to_field_img = PhotoImage(file=self.FIELD_IMG)
+        self.to_field_bg = self.canvas.create_image(
+            315.0, 139.5,
+            image=self.to_field_img
+        )
+
+        self.to_field = TextField(
+            master=self.frame,
+            genesis_root=self.root,
+            placeholder_text="usd",
+            bd=0,
+            bg="#ffffff",
+            highlightthickness=0
+        )
+
+        self.to_field.place(
+            x=254.5, y=127,
+            width=121.0,
             height=27
         )
 
@@ -65,22 +89,30 @@ class AutomationPage(Page):
             image=self.done_btn_img,
             borderwidth=0,
             highlightthickness=0,
-            command=self.update_chart,
+            command=self.place_update_chart,
             relief="flat"
         )
 
         self.done_btn.place(
-            x=275, y=120,
+            x=422, y=120,
             width=39,
             height=39
         )
 
-        self.result = eth_generic_functions.get_coin_price_data(
-            coin_id=self.id_field.text,
-            to="eur",
-            days=1
+        self.place_update_chart()
+
+        # https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=eur&days=7
+        # https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=eur&days=4&interval=daily
+        # https://api.coingecko.com/api/v3/coins/list
+
+    def place_update_chart(self):
+        result = eth_generic_functions.get_coin_price_data(
+            coin_id=self.id_field.text.lower().strip(),
+            to=self.to_field.text.lower().strip(),
+            days=365
         )
 
+        self.graph_frame.destroy()
         self.graph_frame = Frame(self.frame)
         self.graph_frame.place(
             x=60, y=196.5
@@ -88,17 +120,11 @@ class AutomationPage(Page):
 
         self.price_chart = BasicChart(
             master=self.graph_frame,
-            data=self.result["prices"],
+            data=result["prices"],
             kind="line",
             # if first value is greater than last value it means that the asset has lost value
-            color="red" if self.result["prices"][0][1] > self.result["prices"][len(self.result["prices"]) - 1][1] else "green",
-            title=f"{self.id_field.text} / eur",
+            color="red" if result["prices"][0][1] > result["prices"][len(result["prices"]) - 1][
+                1] else "green",
+            title=f"{self.id_field.text.lower().strip()} / {self.to_field.text.lower().strip()}",
         )
         self.price_chart.place_chart()
-
-        # https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=eur&days=7
-        # https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=eur&days=4&interval=daily
-        # https://api.coingecko.com/api/v3/coins/list
-
-    def update_chart(self):
-        print("update")
